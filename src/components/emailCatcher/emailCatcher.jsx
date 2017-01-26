4c2ee48321996315bbaf91596801676cfb543c4a
Conor Macken <cwmacken@gmail.com>
2017-01-26 13:27:02 -0800

    cleaned up email catcher

--- src/components/emailCatcher/emailCatcher.jsx
+++ src/components/emailCatcher/emailCatcher.jsx
@@ -13,7 +13,8 @@ const styles = StyleSheet.create({
     'font-weight': '300',
     'line-height': '1.5',
     'font-family': '-apple-system, BlinkMacSystemFont, Lato, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
-    'margin-top': '7px'
+    'margin-top': '7px',
+    display: 'block'
 
   },
   input: {
@@ -23,11 +24,10 @@ const styles = StyleSheet.create({
 })
 
 
-export default class Navbarcomp extends React.Component {
+export default class EmailCatcher extends React.Component {
   render() {
     return (
       <div>
-        <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css"></link>
         <div id="mc_embed_signup">
           <form action="//conormacken.us15.list-manage.com/subscribe/post?u=c55cbcb3b82621b0969d71f42&amp;id=cbd15b684b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
             <div className={css(styles.input)}  id="mc_embed_signup_scroll">
