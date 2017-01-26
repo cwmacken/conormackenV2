a25838f429476209e95a20472b1aca33256e0168
Conor Macken <cwmacken@gmail.com>
2017-01-26 13:26:47 -0800

    consolidated animated buttons

---
+++ src/components/animatedButton/animatedButton.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+require("./animatedButton.less")
+import { Link } from 'react-router'
+
+class AnimatedButton extends React.Component {
+
+  componentWillMount(){
+    this.setState({loading: true})
+  }
+
+  componentDidMount() {
+    var self = this;
+    setTimeout(() => {
+      self.setState({loading: false});
+    }, self.props.timeout);
+  }
+
+  render() {
+
+    if (this.state.loading) {
+        return (
+          <div />
+        )
+    } else {
+      if(this.props.link){
+        return (
+            <ReactCSSTransitionGroup transitionName={this.props.name} transitionAppear={true} transitionAppearTimeout={2000} transitionEnter={false} transitionLeave={false}>
+              <div>
+                <a target="_blank" href="http://conormacken.com/blog">
+                    <h3 key={this.props.name} className={this.props.style}>{this.props.name}</h3>
+                </a>
+              </div>
+            </ReactCSSTransitionGroup>
+        )
+      }else{
+        return (
+            <Link to={this.props.url}>
+              <ReactCSSTransitionGroup transitionName={this.props.name} transitionAppear={true} transitionAppearTimeout={2000} transitionEnter={false} transitionLeave={false}>
+
+                    <h3 key={this.props.name} className={this.props.style}>{this.props.name}</h3>
+
+              </ReactCSSTransitionGroup>
+            </Link>
+        )
+      }
+    }
+  }
+}
+
+export default AnimatedButton
