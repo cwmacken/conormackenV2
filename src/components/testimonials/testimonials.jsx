4bfdd55fcf9c7546decfbf4f5dec9393f2d9f3dd
Conor Macken <cwmacken@gmail.com>
2017-05-18 12:18:03 +0800

    Caroline testimonial

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -8,6 +8,12 @@ import ExampleUnit from '../exampleUnit/exampleUnit.jsx'
 
 const examples = [
   {
+    title: "Andy Gossett - Corsurf.com",
+    content: "Conor was key in completely revamping our website, which had an immediate impact on our conversion rate. He has a deep understanding of the both the technical side, while still understanding the importance of the look and flow of a website.  His expertise was invaluable starting from the big picture of how to best build a successful website.",
+    url: "https://www.linkedin.com/in/andy-gossett-5760066b/",
+    linkTitle: "Linkedin"
+  },
+  {
     title: "Kevin Tague",
     content: "In his time at DIRECTV’s Digital Innovation Lab (DLab), Conor quickly grew into an indispensable member of the prototype development team. He is a solid developer, a quick learner, and a creative problem solver. The enthusiasm and ingenuity he brings to his work were reflected in the cross-platform entertainment solutions he helped develop for web, mobile, and TV.",
     url: "https://www.linkedin.com/in/kevin-tague-1a31092/",
@@ -20,10 +26,10 @@ const examples = [
     linkTitle: "Linkedin"
   },
   {
-    title: "Andy Gossett - Corsurf.com",
-    content: "Conor was key in completely revamping our website, which had an immediate impact on our conversion rate. He has a deep understanding of the both the technical side, while still understanding the importance of the look and flow of a website.  His expertise was invaluable starting from the big picture of how to best build a successful website.",
-    url: "https://www.linkedin.com/in/andy-gossett-5760066b/",
-    linkTitle: "Linkedin"
+    title: "Caroline Griswold",
+    content: "I asked Conor to build my personal website. I have close to zero technical skills, but he was patient with me in explaining basic html and why he made specific technical decisions. My site is up and running and is easy to manage while still looking very professional.",
+    url: "https://thecarolinegriswold.com/",
+    linkTitle: "Blog / Portfolio Site"
   }
 
 ]
